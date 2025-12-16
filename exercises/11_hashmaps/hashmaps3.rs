6563ca81558d52e792aaef8b863819781a544ace
Josh <jtanjoshua@gmail.com>
2025-12-16 23:01:17 +0800

    hashmaps3

--- exercises/11_hashmaps/hashmaps3.rs
+++ exercises/11_hashmaps/hashmaps3.rs
@@ -27,10 +27,20 @@ fn build_scores_table(results: &str) -> HashMap<&str, TeamScores> {
         let team_1_score: u8 = split_iterator.next().unwrap().parse().unwrap();
         let team_2_score: u8 = split_iterator.next().unwrap().parse().unwrap();
 
-        // TODO: Populate the scores table with the extracted details.
-        // Keep in mind that goals scored by team 1 will be the number of goals
-        // conceded by team 2. Similarly, goals scored by team 2 will be the
-        // number of goals conceded by team 1.
+        let team1_scores = scores.entry(team_1_name).or_insert(TeamScores {
+            goals_scored: 0,
+            goals_conceded: 0,
+        });
+
+        team1_scores.goals_scored += team_1_score;
+        team1_scores.goals_conceded += team_2_score;
+
+        let team2_scores = scores.entry(team_2_name).or_insert(TeamScores {
+            goals_scored: 0,
+            goals_conceded: 0,
+        });
+        team2_scores.goals_scored += team_2_score;
+        team2_scores.goals_conceded += team_1_score;
     }
 
     scores
@@ -54,9 +64,11 @@ England,Spain,1,0";
     fn build_scores() {
         let scores = build_scores_table(RESULTS);
 
-        assert!(["England", "France", "Germany", "Italy", "Poland", "Spain"]
-            .into_iter()
-            .all(|team_name| scores.contains_key(team_name)));
+        assert!(
+            ["England", "France", "Germany", "Italy", "Poland", "Spain"]
+                .into_iter()
+                .all(|team_name| scores.contains_key(team_name))
+        );
     }
 
     #[test]
