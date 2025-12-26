4fb6833bc9509d93d8e259a4094894dcc366e996
Josh <jtanjoshua@gmail.com>
2025-12-26 17:06:43 +0800

    threads1

--- exercises/20_threads/threads1.rs
+++ exercises/20_threads/threads1.rs
@@ -24,6 +24,10 @@ fn main() {
     for handle in handles {
         // TODO: Collect the results of all threads into the `results` vector.
         // Use the `JoinHandle` struct which is returned by `thread::spawn`.
+        match handle.join() {
+            Ok(res) => results.push(res),
+            Err(_) => {}
+        }
     }
 
     if results.len() != 10 {
