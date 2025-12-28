3fdf4fd8308779384733daed20c765db2c08a384
Josh <jtanjoshua@gmail.com>
2025-12-28 22:26:59 +0800

    from_str

--- exercises/23_conversions/from_str.rs
+++ exercises/23_conversions/from_str.rs
@@ -41,7 +41,22 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {}
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut tokens = s.split(",");
+        let (Some(name), Some(age), None) = (tokens.next(), tokens.next(), tokens.next()) else {
+            return Err(ParsePersonError::BadLen);
+        };
+
+        if name.is_empty() {
+            return Err(ParsePersonError::NoName);
+        }
+        let parsed_age = age.parse().map_err(ParsePersonError::ParseInt)?;
+
+        Ok(Person {
+            name: name.into(),
+            age: parsed_age,
+        })
+    }
 }
 
 fn main() {
